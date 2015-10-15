cbcd9b36ca383571809a721cfe9305b5a95de48a
nil1666 <register@mobile-app-insight.org>
2015-10-15 10:48:57 +0800

    new

---
+++ testcase/DoS/F_DOS_01/gen/testcase/t_dos_01/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package testcase.t_dos_01;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ testcase/DoS/T_DOS_01/src/testcase/t_dos_01/MainActivity.java
@@ -0,0 +1,50 @@
+package testcase.t_dos_01;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Intent mIntent = this.getIntent();
+		if(mIntent !=null && mIntent.getExtras() != null && mIntent.getExtras().containsKey("testcase")){
+			TestClass mTestClass = mIntent.getParcelableExtra("testcase");
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	class TestClass implements Parcelable{
+		TestClass(){
+			
+		}
+
+		@Override
+		public int describeContents() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public void writeToParcel(Parcel dest, int flags) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+}
